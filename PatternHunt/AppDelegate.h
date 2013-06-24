ed6b67b41e807486ecab63ab17a5632609823fa5
alpkeser <yalpkeser@gmail.com>
2013-06-24 09:02:48 -0700

    Initial Commit

---
+++ PatternHunt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PatternHunt
+//
+//  Created by alp keser on 6/24/13.
+//  Copyright (c) 2013 alp keser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
